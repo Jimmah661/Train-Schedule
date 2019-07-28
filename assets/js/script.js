0ed8c6af30fa580866d3f506262f1913165d99cf
James Rudd <james.rudd45@gmail.com>
2019-07-28 17:03:54 +1000

    form setup complete

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCU9OtgZ3fqyQulZ_mEDmu01pnmfCJ3j-c",
+    authDomain: "train-timetable-77fa2.firebaseapp.com",
+    databaseURL: "https://train-timetable-77fa2.firebaseio.com",
+    projectId: "train-timetable-77fa2",
+    storageBucket: "",
+    messagingSenderId: "1022215882729",
+    appId: "1:1022215882729:web:76b388fcc1814688"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
