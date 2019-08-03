0ff719ce8dc0685024c051ffe4c654c594013441
James Rudd <james.rudd45@gmail.com>
2019-08-03 20:34:48 +1000

    Final Commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,7 +21,6 @@ $('button').on('click', function (event) {
     var first = moment(($('#firstTime').val().trim()), 'HH:mm').format('HH:mm')
     var freq = $('#frequency').val().trim();
 
-    console.log(name, dest, first, freq);
     $('#trainName').val("");
     $('#destination').val("");
     $('#firstTime').val("");
@@ -35,39 +34,27 @@ $('button').on('click', function (event) {
     });
 });
 
+// Populate table on page load
 database.ref('/newTrain').on('child_added', function (snap) {
     var trainRow = $('<tr>');
-    var trainTime = moment(snap.val().first, "HH:mm")
-    // var currentTime = moment().format('HH:mm');
-    var difference = moment().diff(trainTime, 'minutes');
-    var remainingTime = parseInt(snap.val().freq) - (difference % parseInt(snap.val().freq));
-    var nextArrival = moment().add(remainingTime, 'minutes').format('LT');
-    console.log(trainTime);
-    // console.log("Current Time: " + currentTime);
-    console.log("difference: " + difference);
-    console.log(remainingTime)
-    console.log(moment().add(remainingTime, 'minutes'))
-    console.log(moment().add(remainingTime, 'minutes').format('LT'))
 
-    // Steps to work out the Time till next and the minutes remaining
-    // minus the initial time from the current time
-    // times the number of hours by 60 and add to the minutes to get minutes elapsed
-    // get the modulus remainder of the minutes elapsed by the frequency
-    // times the modulus remainder by the frequency
-    // minus the modulus*frequency from the minutes elapsed to get time till next train
-    // minus the time till next train from the frequency and add to current time to get the next departure time
+    //SAME AS REMAININGTIME BELOW, JUST READABLE
+    // var trainTime = moment(snap.val().first, "HH:mm")
+    // var difference = moment().diff(trainTime, 'minutes');
+    // var remainingTime = parseInt(snap.val().freq) - (difference % parseInt(snap.val().freq));
+
+    var remainingTime = parseInt(snap.val().freq) - ((moment().diff(moment(snap.val().first, "HH:mm"), 'minutes')) % parseInt(snap.val().freq));
 
     trainRow.append($('<td>' + snap.val().name + '</td>'));
     trainRow.append($('<td>' + snap.val().dest + '</td>'));
     trainRow.append($('<td>' + snap.val().freq + '</td>'));
-    trainRow.append($('<td>' + nextArrival + '</td>'));
+    trainRow.append($('<td>' + moment().add(remainingTime, 'minutes').format('LT') + '</td>'));
     if (remainingTime == 0) {
         trainRow.append($('<td>Arriving Now!</td>'))
     }
     else {
         trainRow.append($('<td>' + remainingTime + ' Minutes</td>'));
     }
-
 
     $('#trainList').append(trainRow)
 });
